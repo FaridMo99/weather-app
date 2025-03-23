0ba7bdfc8fa929a48554df78e638b8d6b32c2742
FaridMo99 <farid-ahmad@hotmail.de>
2025-03-23 20:32:32 +0100

    finished weather app

--- src/main.js
+++ src/main.js
@@ -3,12 +3,35 @@ import "./style.css";
 const form = {
   text: document.querySelector(".input"),
   searchButton: document.querySelector(".search"),
+  apiKey: "8ZD7AFSAHS2FQ3LEQUKH87PDN",
 
   search() {
-    this.searchButton.addEventListener("click", () => {
+    this.searchButton.addEventListener("click", (e) => {
+      e.preventDefault()
       if (this.text.checkValidity()) {
-        localStorage.setItem("location", this.text.value);
-        //search algorithm
+        const location = this.text.value.trim();
+        sessionStorage.setItem("location", location);
+
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${this.apiKey}&contentType=json`;
+
+        fetch(url, { mode: "cors" })
+          .then(response => response.json())
+          .then(response => {
+            if (response.currentConditions) {
+              const formattedResult = Object.entries(response.currentConditions)
+              .map(([key, value]) => `<div><b>${key}:</b>  <span>${value}</span></div>`).join("")
+              results.container.innerHTML = formattedResult;
+
+              sessionStorage.setItem("result", formattedResult);
+            } else {
+              results.container.textContent = "Weather data not available.";
+            }
+          })
+          .then(()=> window.location.reload())
+          .catch(error => {
+            console.error("Error fetching weather data:", error);
+            results.container.textContent = "Failed to fetch weather data.";
+          });
       }
     });
   },
@@ -21,5 +44,14 @@ const results = {
 form.search();
 
 window.onload = function () {
-  form.text.value = localStorage.getItem("location");
+  const savedLocation = sessionStorage.getItem("location");
+  const savedResult = sessionStorage.getItem("result");
+
+  if (savedLocation) {
+    form.text.value = savedLocation;
+  }
+
+  if (savedResult) {
+    results.container.innerHTML = savedResult;
+  }
 };
